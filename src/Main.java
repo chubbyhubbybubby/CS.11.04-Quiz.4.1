11ec20e3ce04a4283cd197a862a3bf74472e3335
chubbyhubbybubby <austinc2026@cis.edu.hk>
2023-11-06 12:40:56 +0800

    updated classpaths

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,22 @@ public class Main {
 
     /** 1. Write a method called numberOfVowels that returns the number of vowels in a string.
      Consider a, e, i, o, u as vowels. **/
-    
+    public static int numberOfVowels(String str) {
+        int count = 0;
+        String vowels = "aeiouAEIOU";
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (vowels.contains(String.valueOf(c))) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+
+
+
 
     /** 2. Write a method called notDivisibleBy235 that finds the number of natural numbers not exceeding N
      and not divisible by any of the numbers [2, 3, 5].
@@ -15,6 +30,19 @@ public class Main {
      5 - divides integer by 5
      Answer: 1 (only one number doesn't divide integer by any of 2, 3, 5) **/
 
+    public static int notDivisibleBy235(int N) {
+        int count = 0;
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+
+
 
     /** 3. Write a method called camelCaseMe so that it converts dash/underscore delimited words into camel casing.
      * The first word within the output should be capitalized only if the original word was capitalized
@@ -26,4 +54,7 @@ public class Main {
      "The_Stealth-Warrior" gets converted to "TheStealthWarrior" **/
 
 
-}
+
+
+
+
